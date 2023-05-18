c488dd88553ea6bcfd3ba1b0c4cee20489ea376c
Owallah <mikesonowallah@gmail.com>
2023-05-18 10:12:24 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
 
+    #setter method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    #getter method
+    def name
+        @this_dogs_name
+    end
+
 end
+
+simba = Dog.new
+simba.name = "Simba"
+puts simba.name
